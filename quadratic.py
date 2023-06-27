079fd814c28565d26790a3d65a678971108c2081
least_smart <tanaumbutt@gmail.com>
2023-06-27 17:10:05 +0500

    Update quadratic.py
    Added a line to tell the user on what to do if there is no variable

--- quadratic.py
+++ quadratic.py
@@ -2,6 +2,7 @@
 from math import sqrt
 
 print('NOTE: IF THERE IS NO NUMBER A VARIABLE, JUST WRITE 1 ALONG WITH WHATEVER SIGN IT HAS')
+print("IF ONE OF THE VARIABLES JUST ISN'T GIVEN WRITE 0 IN ITS PLACE")
 
 a = int(input('Enter a: '))
 b = int(input('Enter b: '))
