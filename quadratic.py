419a0df9cefb46c406c1ce976bd5826c58b2001b
least_smart <tanaumbutt@gmail.com>
2023-06-27 17:05:46 +0500

    Create quadratic.py

---
+++ quadratic.py
@@ -0,0 +1,13 @@
+#ax2 + bx + c = 0
+from math import sqrt
+
+print('NOTE: IF THERE IS NO NUMBER A VARIABLE, JUST WRITE 1 ALONG WITH WHATEVER SIGN IT HAS')
+
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+c = int(input('Enter c: '))
+
+x1 = ((-1*b) + sqrt((b ** 2) - (4*a*c))) / 2*a
+
+x2 = ((-1*b) - sqrt((b ** 2) - (4*a*c))) / 2*a
+print(f'{x1:.2f}, {x2:.2f}')
